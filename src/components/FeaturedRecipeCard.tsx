c2a3342f94310c1d9d637cbd923f31dd34c86741
Reyhansyah11 <mr.reihansyah@gmail.com>
2024-10-27 23:23:18 +0700

    commit 8

--- src/components/FeaturedRecipeCard.tsx
+++ src/components/FeaturedRecipeCard.tsx
@@ -10,10 +10,9 @@ export default function FeaturedRecipeCard({
       <div>
         <div className="card">
           <div className="relative w-[200px] h-[280px] rounded-[30px] bg-white overflow-hidden">
-            <div className="gradient-filter absolute w-full h-full bg-[linear-gradient(180deg,rgba(0,0,0,0)40.47%,#000000_81.6%)] " />
               <div className="relative flex flex-col h-full justify-between p-5 z-20">
-               
                 <div className="flex flex-col gap-[6px] z-10">
+            <div className="gradient-filter absolute w-full h-full bg-[linear-gradient(180deg,rgba(0,0,0,0)40.47%,#000000_81.6%)] " />
                   <h3 className="font-bold text-xl leading-[28px] text-white">
                     {recipe.name}
                   </h3>
@@ -26,16 +25,6 @@ export default function FeaturedRecipeCard({
                   className="absolute left-1 top-0 w-full h-full object-cover"
                   alt="thumbnails"
                 />
-                <div className="flex shrink-0 items-center w-fit rounded-full py-1 px-2 bg-white/20 backdrop-blur">
-                  <img
-                    src="/assets/images/icons/Star 1.svg"
-                    className="w-4 h-4"
-                    alt="star"
-                  />
-                  <span className="font-semibold text-xs leading-[18px] text-white">
-                    4.5
-                  </span>
-                </div>
               </div>
           </div>
         </div>
