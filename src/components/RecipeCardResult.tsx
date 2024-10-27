3a7baae867478745c3586eccedfe7f88a92c8d32
Reyhansyah11 <mr.reihansyah@gmail.com>
2024-10-27 22:14:36 +0700

    first commit

--- src/components/RecipeCardResult.tsx
+++ src/components/RecipeCardResult.tsx
@@ -6,7 +6,6 @@ export default function RecipeCardResult({ recipe }: RecipeCardResultProps) {
   return (
     <>
       <div className="card">
-        <div className="flex rounded-[20px] p-[14px] gap-[14px] bg-white shadow-[0_12px_30px_0_#D6D6D640] transition-all duration-300 hover:shadow-[0_10px_20px_0_#FF4C1C80]">
           <div className="flex shrink-0 w-[100px] h-20 rounded-[20px] overflow-hidden bg-[#D9D9D9]">
             <img
               src={`${baseUrl}/${recipe.thumbnail}`}
@@ -14,6 +13,7 @@ export default function RecipeCardResult({ recipe }: RecipeCardResultProps) {
               alt="thumbnail"
             />
           </div>
+        <div className="flex rounded-[20px] p-[14px] gap-[14px] bg-white shadow-[0_12px_30px_0_#D6D6D640] transition-all duration-300 hover:shadow-[0_10px_20px_0_#FF4C1C80]">
           <div className="flex flex-col w-full">
             <div className="flex items-center justify-between">
               <h3 className="font-bold text-lg leading-[24px]">
