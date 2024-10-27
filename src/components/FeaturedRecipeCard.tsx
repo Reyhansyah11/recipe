b3787041863fa4218a6ffc3bf2ed6f34819ba16d
Reyhansyah11 <mr.reihansyah@gmail.com>
2024-10-27 23:31:19 +0700

    commit 8

--- src/components/FeaturedRecipeCard.tsx
+++ src/components/FeaturedRecipeCard.tsx
@@ -10,22 +10,22 @@ export default function FeaturedRecipeCard({
       <div>
         <div className="card">
           <div className="relative w-[200px] h-[280px] rounded-[30px] bg-white overflow-hidden">
-              <div className="relative flex flex-col h-full justify-between p-5 z-20">
-                <img
-                  src={`${baseUrl}/${recipe.thumbnail}`}
-                  className="absolute left-1 top-0 w-full h-full object-cover"
-                  alt="thumbnails"
-                />
-            <div className="gradient-filter absolute w-full h-full bg-[linear-gradient(180deg,rgba(0,0,0,0)40.47%,#000000_81.6%)] z-10" />
-                <div className="flex flex-col gap-[6px] z-10">
-                  <h3 className="font-bold text-xl leading-[28px] text-white">
-                    {recipe.name}
-                  </h3>
-                  <p className="font-semibold text-xs leading-[18px] text-[#FF4C1C]">
-                    {recipe.category.name}
-                  </p>
-                </div>
+            <div className="relative flex flex-col h-full justify-between p-5 z-20">
+              <img
+                src={`${baseUrl}/${recipe.thumbnail}`}
+                className="absolute left-1 top-0 w-full h-full object-cover z-20"
+                alt="thumbnails"
+              />
+              <div className="gradient-filter absolute w-full h-full bg-[linear-gradient(180deg,rgba(0,0,0,0)40.47%,#000000_81.6%)] z-10" />
+              <div className="flex flex-col gap-[6px] z-10">
+                <h3 className="font-bold text-xl leading-[28px] text-white">
+                  {recipe.name}
+                </h3>
+                <p className="font-semibold text-xs leading-[18px] text-[#FF4C1C]">
+                  {recipe.category.name}
+                </p>
               </div>
+            </div>
           </div>
         </div>
       </div>
