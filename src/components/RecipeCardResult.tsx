e4ec5ad883201a35df249cbf6714d20de30a9328
Reyhansyah11 <mr.reihansyah@gmail.com>
2024-10-29 07:16:14 +0700

    update commit 8

--- src/components/RecipeCardResult.tsx
+++ src/components/RecipeCardResult.tsx
@@ -7,11 +7,12 @@ export default function RecipeCardResult({ recipe }: RecipeCardResultProps) {
     <>
       <div className="card">
         <div className="flex rounded-[20px] p-[14px] gap-[14px] bg-white shadow-[0_12px_30px_0_#D6D6D640] transition-all duration-300 hover:shadow-[0_10px_20px_0_#FF4C1C80]">
-          <div className="flex shrink-0 w-[100px] h-20 rounded-[20px] overflow-hidden bg-[#D9D9D9] ">
+          <div className="flex shrink-0 w-[100px] h-20 rounded-[20px] overflow-hidden bg-[#D9D9D9] aspect-square">
             <img
               src={`${baseUrl}/${recipe.thumbnail}`}
               className="w-full h-full object-cover"
               alt="thumbnail"
+              style={{ objectFit: "cover" }}
             />
           </div>
           <div className="flex flex-col w-full">
